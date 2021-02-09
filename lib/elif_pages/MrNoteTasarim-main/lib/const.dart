64dc5e1da6c27eef86c78c530550cb1dca5f014a
elifbilgep <elf.prlk64@gmail.com>
2021-02-09 16:32:10 +0300

    elif_pages altında tasarım sayfaları eklnedi

---
+++ lib/elif_pages/MrNoteTasarim-main/lib/const.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+var headerStyle =
+    TextStyle(fontFamily: "Aileron", fontSize: 50, color: Colors.grey.shade800);
+
+var headerStyle2 = TextStyle(
+    fontFamily: "Aileron",
+    fontSize: 20,
+    fontWeight: FontWeight.w200,
+    color: Colors.grey.shade600);
+
+var headerStyle3 = TextStyle(
+    fontFamily: "Aileron",
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colors.grey.shade800);
+
+var headerStyle4 =
+    TextStyle(fontFamily: "Aileron", fontSize: 15, color: Colors.grey.shade700);
+
+var headerStyle5 = TextStyle(
+    fontFamily: "Aileron",
+    fontSize: 15,
+    color: Colors.grey.shade800,
+    fontWeight: FontWeight.bold);
+
+var headerStyle6 =
+    TextStyle(fontFamily: "Aileron", fontSize: 50, color: Colors.white);
+
+var headerStyle7 = TextStyle(
+    fontFamily: "Aileron", fontSize: 15, color: Colors.white.withOpacity(0.7));
+
+var headerStyle8 = TextStyle(
+    fontFamily: "Aileron",
+    fontSize: 15,
+    color: Colors.white.withOpacity(0.7),
+    fontWeight: FontWeight.bold);
+
+var headerStyle9 = TextStyle(
+    fontFamily: "Aileron",
+    fontSize: 20,
+    color: Colors.white.withOpacity(0.7),
+    fontWeight: FontWeight.bold);
+
+var headerStyle10 = TextStyle(
+    fontFamily: "Aileron",
+    fontSize: 15,
+    color: Colors.grey.shade500,
+    fontWeight: FontWeight.bold);
+
+var scaffoldColor = Color.fromRGBO(239, 239, 239, 1);
+
+Color generalColor = Color.fromRGBO(176, 140, 249, 1);
+
+Color sportColor = Color.fromRGBO(162, 228, 173, 1);
+Color familyColor = Color.fromRGBO(249, 164, 164, 1);
+Color schoolColor = Color.fromRGBO(253, 191, 255, 1);
+Color businessColor = Color.fromRGBO(211, 238, 251, 1);
+
+BorderRadius borderRadis1 = BorderRadius.circular(5);
---
+++ lib/elif_pages/MrNoteTasarim-main/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hakki_tasarim/pages/create_note.dart';
+import 'package:hakki_tasarim/pages/note_page.dart';
+
+import 'pages/home.dart';
+import 'pages/setting.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Projem',
+      theme: ThemeData(
+        primaryColor: Colors.grey.shade600,
+      ),
+      home: Notes_Page(),
+    );
+  }
+}
