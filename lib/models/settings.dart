7ac6ac85e72f0a2811c161f2869de2789f67a174
HAKKICAN <hakkicanbuluc@gmail.com>
2021-07-14 17:58:54 +0300

    last updated before updating v 2.5.0

--- lib/models/settings.dart
+++ lib/models/settings.dart
@@ -8,7 +8,7 @@ class Settings {
   Color currentColor;
   static Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   bool adOpen;
-  static bool test = true;
+  static bool test = false;
   String gelistiriciSayfasiParola = "Hkcblc48";
   static final String appIDCanli = "ca-app-pub-2104543393026445~1095002395";
   static final String gecis1Canli = "ca-app-pub-2104543393026445/8249430070";
